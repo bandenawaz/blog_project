536ba66b700e88438b302c9158e088af7241e042
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-02-09 21:12:22 +0530

    First commit

---
+++ blog_frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Illuminate skills Blog</h1>
+      <p>Welcome to Blogs. </p>
+    </div>
+  );
+}
+
+export default App;
